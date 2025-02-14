30fdbd317cac08a8ba89bac24ca206b6a3b6463d
YXHXianYu <2943003@qq.com>
2025-02-14 13:58:44 +0800

    docs: add docs

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ mod auto_clicker;
 use auto_clicker::AutoClicker;
 
 fn main() {
+    println!("Press `Ctrl+Shift+J` to start auto-clicking");
+    println!("Press again or press `Ctrl+Shift+K` to stop auto-clicking");
     let auto_clicker = AutoClicker::new();
     auto_clicker.start_listener();
 }
